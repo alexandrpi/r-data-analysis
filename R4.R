7b0dd5fd009a978d7e4a9bbea0d2d91ce5da6efd
alexandrpi <alexandr.pipi@gmail.com>
2016-10-22 22:41:14 +0300

    Task 4 completed, report added.

--- R4.R
+++ R4.R
@@ -1,13 +1,51 @@
-sample.norm <- rnorm(1000, 19, 3)
+# нормальное распределение
+# в качесте предполгаемых данных возьмем массу мозга человека
+# средняя масса мозга мужчины: 1350 г.
+sample.norm <- rnorm(1000, 1350, 120)
 hist(sample.norm,
      breaks = "Sturges",
-     col = "#5AA0D6")
+     col = "#1B263B",
+     main = "Нормальное распределение")
 summary(sample.norm)
+boxplot(sample.norm)
 var(sample.norm)
 
-sample.unif <- runif(n = 1000, max = 5, min = 17)
+# равномерное распределение
+sample.unif <- runif(1000, min = 5, max = 17)
 hist(sample.unif,
      breaks = "Sturges",
-     col = "#EF8B6B")
-summary(sample.norm)
-var(sample.norm)
+     col = "#242331",
+     main = "Равномерное распределение")
+summary(sample.unif)
+boxplot(sample.unif)
+var(sample.unif)
+
+# экспоненциальное распределение
+sample.exp <- rexp(1000, 4)
+hist(sample.exp,
+     breaks = "Sturges",
+     col = "#F6CA83",
+     main = "Экспоненциальное распределение")
+summary(sample.exp)
+boxplot(sample.exp)
+var(sample.exp)
+
+# распределение Стьюдента
+sample.t <- rt(1000, 4)
+hist(sample.t,
+     breaks = "Sturges",
+     col = "#283F3B",
+     main = "Распределение Стьюдента")
+summary(sample.t)
+boxplot(sample.t)
+var(sample.t)
+
+# распределение Хи-квадрат
+sample.chisq <- rchisq(1000, 7)
+hist(sample.chisq,
+     breaks = "Sturges",
+     col = "#9CAFB7",
+     main = "Распределение Хи-квадрат")
+summary(sample.chisq)
+boxplot(sample.chisq)
+var(sample.chisq)
