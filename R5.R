6d18cb72f04750c8db93f3d4f5e4d97638c6f20d
alexandrpi <alexandr.pipi@gmail.com>
2016-12-08 08:36:58 +0300

    5-th-task-regression-done
    Finally done the regression

--- R5.R
+++ R5.R
@@ -1,20 +1,28 @@
-remove(sqft_living, sample.chisq, sample.exp, sample.t, sample.unif)
-data <- read.table("kc_house_data.csv",
+remove(regression, regression.fixed, price, sqft.living)
+data <- read.table("cust_less_1800_cnt_wkds.txt",
                    header=TRUE,
-                   sep=",")
-data <- na.omit(subset(data, sqft_living < 7000))
+                   sep=";")
+data.duration <- read.table("DATA_ALL_wkds.txt",
+                   header=TRUE,
+                   sep=";")
+na.fail(data)
+data <- na.omit(data)
+data.duration <- subset(subset(data.duration, duration < 1800), subscription_type=="Customer")
 options(scipen=5)
-price <- data[,3]
-sqft.living <- data[,6]
-plot(sqft.living, price, col="#2C666E", type="p",
-     xlab="Жилая площадь",
-     ylab="Цена на квартиру")
-
-regression <- lm(formula=price ~ sqft_living)
+cor(data[c(2:22)])
+cor(data.duration[c(3:19, 21:23)])
+count <- data[,22]
+mean_temp <- data[,3]
+temp.min <- min(mean_temp)
+temp.max <- max(mean_temp)
+temp.diff <- temp.max - temp.min
+mean_temp <- mapply(function(temp) (temp - temp.min) / temp.diff, mean_temp)
+plot(mean_temp, count, col="#75B9BE", type="p",
+     pch=16,
+     xlab="Средняя температура воздуха, F",
+     ylab="Количество поездок")
+regression <- lm(formula=count ~ mean_temp)
 regression
-regression.fixed <- lm(formula=price ~ sqft_living - 1)
-regression.fixed
 summary(regression)
 summary(regression.fixed)
-abline(regression, col="#3C0000", lwd="3")
-abline(regression.fixed, col="#D69F7E", lwd="3")
+abline(regression, col="#EE7674", lwd="3")
