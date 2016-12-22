cb0d6c74705583a669034512ac0eee7ed01bb632
alexandrpi <alexandr.pipi@gmail.com>
2016-12-22 10:59:15 +0300

    6-task-without-report
    Done task 6

--- R5.R
+++ R5.R
@@ -1,20 +1,36 @@
-remove(sqft_living, sample.chisq, sample.exp, sample.t, sample.unif)
-data <- read.table("kc_house_data.csv",
+remove(duration, data.cust, data.summer, data.sub, max_temp)
+data <- read.table("cust_less_1800_cnt_wkds.txt",
                    header=TRUE,
-                   sep=",")
-data <- na.omit(subset(data, sqft_living < 7000))
+                   sep=";")
+data.duration <- read.table("DATA_ALL_wkds.txt",
+                   header=TRUE,
+                   sep=";")
+na.fail(data)
+data <- na.omit(data)
+data.duration <- subset(subset(data.duration, duration < 1800), subscription_type=="Customer")
 options(scipen=5)
-price <- data[,3]
-sqft.living <- data[,6]
-plot(sqft.living, price, col="#2C666E", type="p",
-     xlab="Жилая площадь",
-     ylab="Цена на квартиру")
-
-regression <- lm(formula=price ~ sqft_living)
+cor(data[c(2:22)])
+cor(data.duration[c(3:19, 21:23)])
+count <- data[,22]
+mean_temp <- data[,3]
+temp.min <- min(mean_temp)
+temp.max <- max(mean_temp)
+temp.diff <- temp.max - temp.min
+mean_temp <- mapply(function(temp) (temp - temp.min) / temp.diff, mean_temp)
+plot(mean_temp, count, col="#B8B42D", type="p",
+     pch=20,
+     xlab="Средняя температура воздуха, F",
+     ylab="Количество поездок")
+regression <- lm(formula=count ~ mean_temp)
 regression
-regression.fixed <- lm(formula=price ~ sqft_living - 1)
-regression.fixed
 summary(regression)
-summary(regression.fixed)
-abline(regression, col="#3C0000", lwd="3")
-abline(regression.fixed, col="#D69F7E", lwd="3")
+abline(regression, col="#C81D25", lwd="3")
+cor(count, mean_temp)
+prognosis.temps <-c(53, 71, 64, 78, 46)
+prognosis.counts <- predict(regression, data.frame(mean_temp=prognosis.temps), level=0.9, interval="confidence") 
+prognosis.counts
+points(prognosis.temps,
+       prognosis.counts[,1],
+       col="#3E363F",
+       pch=17,
+       add=TRUE)
---
+++ R6.1.R
@@ -0,0 +1,65 @@
+options(scipen=5)
+gym.data <- read.table("data_gym.csv",
+                       header = TRUE,
+                       sep = ",")
+gym.data[, 2] <- mapply(function(timestamp) timestamp / 3600, gym.data[, 2])
+# требуемые нам атрибуты:
+# timestamp (2) — кол-во секунд, прошедших с момента начала суток
+# is_weekend (4) — день недели
+# apparent_temperature (6) — ощущаемая температура воздуха, в фаренгейтах
+# is_start_of_semester (8) — 1 - начало семестра, 0 - нет
+# number_people (1) — количество людей в зале
+cor(gym.data)
+cor(gym.data[, 1], gym.data[, c(2, 4, 6, 8)])
+indices.train <- c(2375:7643, 9840:18466, 20071:26066)
+indices.test <- c(1:2374, 7644:9839, 18467:20070)
+length(indices.test) + length(indices.train)
+gym.data.train <- gym.data[indices.train,]
+gym.data.test <- gym.data[indices.test,]
+
+# СПОСОБ 2
+crowd.mul.regression <- lm(number_people ~ timestamp +
+                             is_weekend +
+                             apparent_temperature +
+                             is_start_of_semester,
+                           gym.data.train)
+summary(crowd.mul.regression)
+crowd.predict <- predict(crowd.mul.regression, gym.data.test)
+crowd.true <- gym.data.test[,1]
+plot(crowd.predict[70:370], crowd.true[70:370], col="#B8B42D", type="p",
+     pch=20,
+     xlab="Предсказанные значения",
+     ylab="Настоящие значения")
+
+# СПОСОБ 1
+f <- 4
+matrix.gym <- matrix(nrow = 0, ncol = f + 1)
+temp <- read.table("data_gym.csv",
+                         header = TRUE,
+                         sep = ",")
+temp[, 2] <- mapply(function(timestamp) timestamp / 3600, temp[, 2])
+matrix.gym <- temp[, c(1, 2, 4, 6, 8)]
+n <- length(matrix.gym[,1])
+# первый столбец отвечает за переменную отклика, возьмем только факторные переменные
+X.gym <- matrix.gym[, 2:(f + 1)]
+Y.gym <- matrix.gym[, 1]
+X1.gym <- matrix(nrow = n, ncol = f + 1)
+for (k in 1:(f + 1)) {
+  print(k)
+  if (k == 1) {
+    X1.gym[, k] <- runif(n, 1:1)
+  }
+  else {
+    X1.gym[, k] <- X.gym[, k - 1]
+  }
+}
+X1.gym.train <- X1.gym[indices.train,]
+T <- t(X1.gym.train) %*% X1.gym.train 
+library(MASS)
+T.inverse <- ginv(T)
+th <- (T.inverse%*%t(X1.gym.train)%*%Y.gym[indices.train])[, 1]
+prognosis <- th%*%t(X1.gym[indices.test,])
+plot(prognosis[70:370], Y.gym[indices.test][70:370], col="#B8B42D", type="p",
+     pch=20,
+     xlab="Предсказанные значения",
+     ylab="Настоящие значения")
