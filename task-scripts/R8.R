01134c3890cec5f440ce58adc6955e06c90d85e2
alexandrpi <alexandr.pipi@gmail.com>
2016-12-26 08:14:54 +0300

    8-th-task-started

---
+++ task-scripts/R8.R
@@ -0,0 +1,26 @@
+data.voice <- read.csv("data/voice.csv",
+                       header = TRUE,
+                       sep = ",")
+
+# перекодируем значение переменной отклика
+# 0 — женщина, 1 — мужчина
+data.voice$label <- factor(data.voice$label,
+                           levels = c("male", "female"),
+                           labels = c(0, 1))
+
+voice.size <- length(data.voice[, 1])
+
+plot(data.voice$meanfreq, data.voice$label, type="n", main="test")
+
+for (i in 1:voice.size) 
+{
+  colour <- ifelse(data.voice$label[i] == 1,
+                   "blue",
+                   "red")
+  points(data.voice$meanfreq[i],
+         data.voice$label[i],
+         type="p",
+         pch=16,
+         col=colour,
+         add="TRUE")
+}
