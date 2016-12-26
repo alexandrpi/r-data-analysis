2036d8872461755de64fe4b5082146085e849f41
alexandrpi <alexandr.pipi@gmail.com>
2016-12-26 08:20:50 +0300

    8-th-task-started

--- task-scripts/R8.R
+++ task-scripts/R8.R
@@ -10,14 +10,14 @@ data.voice$label <- factor(data.voice$label,
 
 voice.size <- length(data.voice[, 1])
 
-plot(data.voice$meanfreq, data.voice$label, type="n", main="test")
+plot(data.voice$maxfun, data.voice$label, type="n", main="test")
 
 for (i in 1:voice.size) 
 {
   colour <- ifelse(data.voice$label[i] == 1,
                    "blue",
                    "red")
-  points(data.voice$meanfreq[i],
+  points(data.voice$maxfun[i],
          data.voice$label[i],
          type="p",
          pch=16,
